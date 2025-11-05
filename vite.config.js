84163c7c1f5c370aebde3b4db4804421b1156cff
PattarapongHongkanjanagul <66010615@kmitl.ac.th>
2025-11-05 23:27:00 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,3 +8,17 @@ export default defineConfig({
         open: true,   // เปิดเบราว์เซอร์อัตโนมัติ
     },
 });
+
+import { resolve } from 'path'
+
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        form: resolve(__dirname, 'form.html'),
+        logs: resolve(__dirname, 'logs.html')
+      }
+    }
+  }
+}
