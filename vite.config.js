2838cc5a38897b1732342acc7db7a7c9d0f6a7be
PattarapongHongkanjanagul <66010615@kmitl.ac.th>
2025-11-06 00:12:22 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,14 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  // dev server เวลา run `npm run dev`
   server: {
     port: 5173,
     open: true,
   },
-
-  // ค่า build สำหรับ Vercel
-  base: '/',          // เสิร์ฟจากรากโดเมน
+  base: '/',
   build: {
-    outDir: 'dist',   // ให้ชัดเจน
+    outDir: 'dist',
     rollupOptions: {
-      // บอก Vite ว่ามีหลายหน้า HTML
       input: {
         main: resolve(__dirname, 'index.html'),
         form: resolve(__dirname, 'form.html'),
