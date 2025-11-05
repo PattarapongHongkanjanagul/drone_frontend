7ad6202079721c856bf6a67e1d0efbbc8d3be0aa
PattarapongHongkanjanagul <66010615@kmitl.ac.th>
2025-11-05 23:17:57 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    // ใส่ค่าเพิ่มได้ภายหลัง; ค่านี้พอสำหรับเริ่ม dev server แล้ว
-    server: {
-        port: 5173,   // ปรับได้
-        open: true,   // เปิดเบราว์เซอร์อัตโนมัติ
-    },
-});
+// vite.config.js / .ts
+import { resolve } from 'path'
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main:  resolve(__dirname, 'index.html'),
+        form:  resolve(__dirname, 'form.html'),
+        log:  resolve(__dirname, 'log.html')
+      }
+    }
+  }
+}
